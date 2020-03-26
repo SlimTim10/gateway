92084784bccc73e7ca49f3378f1f4aca32689dc6
SlimTim10 <slimtim10@gmail.com>
2020-03-26 19:17:36 -0400

    Start on serial reading

--- backend/app/Main.hs
+++ backend/app/Main.hs
@@ -1,6 +1,30 @@
 module Main where
 
-import Lib
+import qualified Data.ByteString.Char8 as B
+import System.Hardware.Serialport
+  ( openSerial
+  , closeSerial
+  , defaultSerialSettings
+  , SerialPort(..)
+  , SerialPortSettings(..)
+  , CommSpeed(..)
+  , recv
+  )
+import Control.Concurrent (threadWaitRead, threadDelay)
+
+secondsToMicro :: Int -> Int
+secondsToMicro = (* 1000) . (* 1000)
 
 main :: IO ()
-main = putStrLn "Hello, world!"
+main = do
+  let port = "COM19"          -- Windows
+  s <- openSerial port defaultSerialSettings { commSpeed = CS115200 }
+  loop s
+  closeSerial s
+
+loop :: SerialPort -> IO ()
+loop s = do
+  bs <- recv s 60
+  if B.length bs > 0
+    then print bs >> loop s
+    else loop s
