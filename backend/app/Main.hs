485cd4750ef957c56bd46d6f15efc025caf586bb
SlimTim10 <slimtim10@gmail.com>
2020-03-27 17:13:12 -0400

    Printing the incoming lines

--- backend/app/Main.hs
+++ backend/app/Main.hs
@@ -9,8 +9,11 @@ import System.Hardware.Serialport
   , SerialPortSettings(..)
   , CommSpeed(..)
   , recv
+  , flush
   )
-import Control.Concurrent (threadWaitRead, threadDelay)
+import Control.Concurrent (threadDelay)
+import Text.Printf (printf)
+import Data.Semigroup ((<>))
 
 secondsToMicro :: Int -> Int
 secondsToMicro = (* 1000) . (* 1000)
@@ -18,13 +21,35 @@ secondsToMicro = (* 1000) . (* 1000)
 main :: IO ()
 main = do
   let port = "COM19"          -- Windows
+  -- let port = "/dev/ttyUSB0"  -- Linux
   s <- openSerial port defaultSerialSettings { commSpeed = CS115200 }
+  threadDelay $ secondsToMicro 3
   loop s
   closeSerial s
 
 loop :: SerialPort -> IO ()
 loop s = do
-  bs <- recv s 60
-  if B.length bs > 0
-    then print bs >> loop s
-    else loop s
+  line <- recvLine s
+  case line of
+    Just bs ->  do
+      let cs = B.unpack bs
+      mapM_ (printf "0x%X ") cs
+      putStrLn ""
+      putStrLn cs
+      loop s
+    Nothing -> loop s
+
+recvLine :: SerialPort -> IO (Maybe B.ByteString)
+recvLine = recvLine' ""
+
+recvLine' :: B.ByteString -> SerialPort -> IO (Maybe B.ByteString)
+recvLine' bs s = case lineSuffix `B.stripSuffix` bs of
+  Nothing -> next
+  line -> return line
+  where
+    lineSuffix = "\r\n"
+    next = do
+      b <- recv s 1
+      if B.null b
+        then return Nothing
+        else recvLine' (bs <> b) s
