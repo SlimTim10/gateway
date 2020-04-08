0391dd22334dc3b6a7aa4e5cac921ef6113ef349
SlimTim10 <slimtim10@gmail.com>
2020-04-08 10:29:00 -0400

    Refactor decodeCOBS

--- backend/src/Encoding.hs
+++ backend/src/Encoding.hs
@@ -8,6 +8,7 @@ module Encoding
 
 import qualified Data.ByteString.Char8 as B
 import Data.Char (chr, ord)
+import Data.List (unfoldr)
 
 encode :: B.ByteString -> B.ByteString
 encode = encodeCOBS
@@ -29,12 +30,7 @@ decodeCOBS = B.init . B.concat . map enc . chunks
     enc :: B.ByteString -> B.ByteString
     enc chunk = B.tail chunk `B.snoc` chr 0x00
     chunks :: B.ByteString -> [B.ByteString]
-    chunks = chunks' []
-    chunks' acc xs
-     | B.null xs = acc
-     | otherwise =
-       let
-         x = B.head xs
-         (ys, rest) = B.splitAt (ord x) xs
-         acc' = acc ++ [ys]
-       in chunks' acc' rest
+    chunks = unfoldr $ \xs ->
+      if B.null xs
+      then Nothing
+      else Just $ B.splitAt (ord $ B.head xs) xs
