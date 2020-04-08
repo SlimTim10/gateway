ad7fb54ab12b1161626120c9bf95fbeef33a20a8
SlimTim10 <slimtim10@gmail.com>
2020-04-07 21:26:00 -0400

    Implement COBS decoding

--- backend/src/Encoding.hs
+++ backend/src/Encoding.hs
@@ -5,7 +5,7 @@ module Encoding
 
 import qualified Data.ByteString.Char8 as B
 import Data.List.Split (splitOn)
-import Data.Char (chr)
+import Data.Char (chr, ord)
 
 encode :: B.ByteString -> B.ByteString
 encode = encodeCOBS
@@ -17,3 +17,22 @@ encodeCOBS = B.concat . map enc . chunks
     chunks = B.split '\0'
     enc :: B.ByteString -> B.ByteString
     enc chunk = chr (B.length chunk + 1) `B.cons` chunk
+
+decode :: B.ByteString -> B.ByteString
+decode = decodeCOBS
+
+decodeCOBS :: B.ByteString -> B.ByteString
+decodeCOBS = B.init . B.concat . map enc . chunks
+  where
+    enc :: B.ByteString -> B.ByteString
+    enc chunk = B.tail chunk `B.snoc` chr 0x00
+    chunks :: B.ByteString -> [B.ByteString]
+    chunks = chunks' []
+    chunks' acc xs
+     | B.null xs = acc
+     | otherwise =
+       let
+         x = B.head xs
+         (ys, rest) = B.splitAt (ord x) xs
+         acc' = acc ++ [ys]
+       in chunks' acc' rest
