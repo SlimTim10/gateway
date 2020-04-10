f2042c986b0bed897919567ddf0f4ecff33dda24
SlimTim10 <slimtim10@gmail.com>
2020-04-10 14:28:12 -0400

    Implement COBS encoding

--- server/src/Encoding.hs
+++ server/src/Encoding.hs
@@ -1,8 +1,9 @@
 module Encoding
-  ( encode
-  , decode
-  , encodeCOBS
+  ( encodeCOBS
   , decodeCOBS
+  , encodeCOBSMaxLength
+  , decodeCOBSMaxLength
+  , cobsBoundary
   )
   where
 
@@ -10,22 +11,34 @@ import qualified Data.ByteString.Char8 as B
 import Data.Char (chr, ord)
 import Data.List (unfoldr)
 
-encode :: B.ByteString -> B.ByteString
-encode = encodeCOBS
+encodeCOBSMaxLength :: Int
+encodeCOBSMaxLength = 254
+
+decodeCOBSMaxLength :: Int
+decodeCOBSMaxLength = 255
+
+cobsBoundary :: B.ByteString
+cobsBoundary = B.singleton (chr 0x00)
 
 encodeCOBS :: B.ByteString -> B.ByteString
-encodeCOBS = B.concat . map enc . chunks
+encodeCOBS
+  = B.concat
+  . map enc
+  . chunks
+  . B.take encodeCOBSMaxLength
   where
-    chunks :: B.ByteString -> [B.ByteString]
-    chunks = B.split '\0'
     enc :: B.ByteString -> B.ByteString
     enc chunk = chr (B.length chunk + 1) `B.cons` chunk
-
-decode :: B.ByteString -> B.ByteString
-decode = decodeCOBS
+    chunks :: B.ByteString -> [B.ByteString]
+    chunks = B.split '\0'
 
 decodeCOBS :: B.ByteString -> B.ByteString
-decodeCOBS = B.init . B.concat . map enc . chunks
+decodeCOBS
+  = B.init
+  . B.concat
+  . map enc
+  . chunks
+  . B.take decodeCOBSMaxLength
   where
     enc :: B.ByteString -> B.ByteString
     enc chunk = B.tail chunk `B.snoc` chr 0x00
--- server/test/EncodingSpec.hs
+++ server/test/EncodingSpec.hs
@@ -22,6 +22,16 @@ spec = do
       let ys = fromBytes [0x01, 0x01, 0x01, 0x04, 0x01, 0x01, 0x03]
       encodeCOBS xs `shouldBe` ys
 
+    it "encodes a list of bytes of maximum length" $ do
+      let xs = fromBytes $ replicate encodeCOBSMaxLength 0x01
+      let ys = fromBytes $ 0xFF : replicate (decodeCOBSMaxLength - 1) 0x01
+      encodeCOBS xs `shouldBe` ys
+
+    it "truncates when encoding a list of bytes longer than the maximum length" $ do
+      let xs = fromBytes $ replicate (encodeCOBSMaxLength + 1) 0x01
+      let ys = fromBytes $ 0xFF : replicate (decodeCOBSMaxLength - 1) 0x01
+      encodeCOBS xs `shouldBe` ys
+
   describe "decodeCOBS" $ do
     it "decodes a list of bytes" $ do
       let xs = fromBytes [0x02, 0x45, 0x01, 0x04, 0x2C, 0x4C, 0x79, 0x01, 0x05, 0x40, 0x06, 0x4F, 0x37]
@@ -31,4 +41,14 @@ spec = do
     it "decodes a raw packet" $ do
       let xs = fromBytes [0x01, 0x01, 0x01, 0x04, 0x01, 0x01, 0x03]
       let ys = fromBytes [0x00, 0x00, 0x00, 0x01, 0x01, 0x03]
+      decodeCOBS xs `shouldBe` ys
+
+    it "decodes a list of bytes of maximum length" $ do
+      let xs = fromBytes $ 0xFF : replicate (decodeCOBSMaxLength - 1) 0x01
+      let ys = fromBytes $ replicate encodeCOBSMaxLength 0x01
+      decodeCOBS xs `shouldBe` ys
+
+    it "truncates when decoding a list of bytes longer than the maximum length" $ do
+      let xs = fromBytes $ 0xFF : replicate (decodeCOBSMaxLength + 1) 0x01
+      let ys = fromBytes $ replicate encodeCOBSMaxLength 0x01
       decodeCOBS xs `shouldBe` ys
