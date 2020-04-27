66702e4627f74a68697af74e74c0b908be015985
SlimTim10 <slimtim10@gmail.com>
2020-04-27 17:54:22 -0400

    Check trigger and apply action to state

--- server/src/State.hs
+++ server/src/State.hs
@@ -3,7 +3,10 @@ module State
   -- ) where
   where
 
-import Data.IntMap.Strict (IntMap)
+import Data.IntMap.Strict
+  ( IntMap
+  , (!?)
+  )
 import qualified Data.IntMap.Strict as IntMap
 
 import Types.Prop
@@ -12,6 +15,10 @@ import Types.Prop
 import qualified Types.Prop as Prop
 import Config
   ( ConfigProp(..)
+  )
+import Types.Rule
+  ( Trigger(..)
+  , Action(..)
   )
 
 type State = IntMap Prop
@@ -30,3 +37,21 @@ fromConfigProp cProp = Prop
   , defaultValue = (defaultValue :: ConfigProp -> Prop.Value) cProp
   , value = (defaultValue :: ConfigProp -> Prop.Value) cProp
   }
+
+checkTrigger :: Trigger -> State -> Bool
+checkTrigger
+  ( Trigger { propKey = key, value = tv } )
+  state
+  =
+  case state !? key of
+    Nothing -> False
+    Just prop -> Prop.value prop == tv
+
+applyAction :: Action -> State -> State
+applyAction
+  ( Action { propKey = key, value = av } )
+  state
+  =
+  IntMap.update f key state
+  where
+    f prop = Just $ (prop :: Prop) { value = av }
