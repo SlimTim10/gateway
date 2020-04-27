58b8f56b5aed3b461d7acd9148446e0bed597da9
SlimTim10 <slimtim10@gmail.com>
2020-04-26 21:51:24 -0400

    Syntax

--- server/test/StateSpec.hs
+++ server/test/StateSpec.hs
@@ -85,96 +85,95 @@ spec = do
       result `shouldSatisfy` isRight
       let (Right state) = result
       state `shouldBe`
-        (IntMap.fromAscList $
-          [ ( 1
-            , Prop
-              { name = "Card Spot 1"
-              , description = Just "RFID tag reader"
-              , address = 1
-              , defaultValue = Prop.Int 0
-              , value = Prop.Int 0
-              }
-            )
-          , ( 2
-            , Prop
-              { name = "Card Spot 2"
-              , description = Just "RFID tag reader"
-              , address = 2
-              , defaultValue = Prop.Int 0
-              , value = Prop.Int 0
-              }
-            )
-          , ( 3
-            , Prop
-              { name = "Card Spot 3"
-              , description = Just "RFID tag reader"
-              , address = 3
-              , defaultValue = Prop.Int 0
-              , value = Prop.Int 0
-              }
-            )
-          , ( 4
-            , Prop
-              { name = "South Door"
-              , description = Just "Enter the next room"
-              , address = 0x10
-              , defaultValue = Prop.String "closed"
-              , value = Prop.String "closed"
-              }
-            )
-          , ( 5
-            , Prop
-              { name = "Big Lockbox"
-              , description = Just "Holds card 4"
-              , address = 0x20
-              , defaultValue = Prop.String "locked"
-              , value = Prop.String "locked"
-              }
-            )
-          , ( 6
-            , Prop
-              { name = "Small Lockbox 1"
-              , description = Just "Holds card 2"
-              , address = 0x30
-              , defaultValue = Prop.String "locked"
-              , value = Prop.String "locked"
-              }
-            )
-          , ( 7
-            , Prop
-              { name = "Small Lockbox 2"
-              , description = Just "Holds the hint to the piano chord"
-              , address = 0x31
-              , defaultValue = Prop.String "locked"
-              , value = Prop.String "locked"
-              }
-            )
-          , ( 8
-            , Prop
-              { name = "Button Puzzle"
-              , description = Nothing
-              , address = 0x40
-              , defaultValue = Prop.Int 0
-              , value = Prop.Int 0
-              }
-            )
-          , ( 9
-            , Prop
-              { name = "Mini Piano"
-              , description = Nothing
-              , address = 0x50
-              , defaultValue = Prop.IntList [0,0,0,0,0,0,0,0,0,0,0,0,0]
-              , value = Prop.IntList [0,0,0,0,0,0,0,0,0,0,0,0,0]
-              }
-            )
-          , ( 10
-            , Prop
-              { name = "East Door"
-              , description = Just "Exit the escape room"
-              , address = 0x60
-              , defaultValue = Prop.String "closed"
-              , value = Prop.String "closed"
-              }
-            )
-          ]
-        )
+        IntMap.fromAscList
+        [ ( 1
+          , Prop
+            { name = "Card Spot 1"
+            , description = Just "RFID tag reader"
+            , address = 1
+            , defaultValue = Prop.Int 0
+            , value = Prop.Int 0
+            }
+          )
+        , ( 2
+          , Prop
+            { name = "Card Spot 2"
+            , description = Just "RFID tag reader"
+            , address = 2
+            , defaultValue = Prop.Int 0
+            , value = Prop.Int 0
+            }
+          )
+        , ( 3
+          , Prop
+            { name = "Card Spot 3"
+            , description = Just "RFID tag reader"
+            , address = 3
+            , defaultValue = Prop.Int 0
+            , value = Prop.Int 0
+            }
+          )
+        , ( 4
+          , Prop
+            { name = "South Door"
+            , description = Just "Enter the next room"
+            , address = 0x10
+            , defaultValue = Prop.String "closed"
+            , value = Prop.String "closed"
+            }
+          )
+        , ( 5
+          , Prop
+            { name = "Big Lockbox"
+            , description = Just "Holds card 4"
+            , address = 0x20
+            , defaultValue = Prop.String "locked"
+            , value = Prop.String "locked"
+            }
+          )
+        , ( 6
+          , Prop
+            { name = "Small Lockbox 1"
+            , description = Just "Holds card 2"
+            , address = 0x30
+            , defaultValue = Prop.String "locked"
+            , value = Prop.String "locked"
+            }
+          )
+        , ( 7
+          , Prop
+            { name = "Small Lockbox 2"
+            , description = Just "Holds the hint to the piano chord"
+            , address = 0x31
+            , defaultValue = Prop.String "locked"
+            , value = Prop.String "locked"
+            }
+          )
+        , ( 8
+          , Prop
+            { name = "Button Puzzle"
+            , description = Nothing
+            , address = 0x40
+            , defaultValue = Prop.Int 0
+            , value = Prop.Int 0
+            }
+          )
+        , ( 9
+          , Prop
+            { name = "Mini Piano"
+            , description = Nothing
+            , address = 0x50
+            , defaultValue = Prop.IntList [0,0,0,0,0,0,0,0,0,0,0,0,0]
+            , value = Prop.IntList [0,0,0,0,0,0,0,0,0,0,0,0,0]
+            }
+          )
+        , ( 10
+          , Prop
+            { name = "East Door"
+            , description = Just "Exit the escape room"
+            , address = 0x60
+            , defaultValue = Prop.String "closed"
+            , value = Prop.String "closed"
+            }
+          )
+        ]
